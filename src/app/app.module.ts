b4ea32316dd79051f55cbd98ed37b384eb3c934d
Sawako <saturnoxspace@outlook.com>
2020-02-01 22:43:40 -0600

    [FEATURE]: Added welcome page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './config/app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SidebarModule } from 'ng-sidebar';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   theme: {
     container: {
-      center: true
-    }
+      center: true,
+    },
+    screens: {
+      xs: '599px',
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1920px',
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
