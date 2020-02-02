bb8a506c28997e28d934816e865cb8e1a4bedc50
Sawako <saturnoxspace@outlook.com>
2020-02-02 13:50:25 -0600

    [FEATURE]: Added function to translate the text in several languages. At the moment only English and Spanish. [FEATURE]: Added Loader at the top of the page to show http loads, paths, and app files. [FEATURE: Added translation to the welcome page. [FEATURE]: Added CSS Framework of icons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private langDefault: string;
+  private langs: string[] = [];
   private _opened: boolean = false;
 
-  private _toggleSidebar() {
+  constructor(private readonly translateService: TranslateService) {
+    if (localStorage.getItem('lang') !== null) {
+      this.translateService.use(localStorage.getItem('lang'));
+      this.langDefault = localStorage.getItem('lang');
+    } else {
+      this.translateService.use('en_US');
+      this.langDefault = 'en_US';
+      localStorage.setItem('lang', 'en_US');
+    }
+
+    this.translateService.setDefaultLang('en_US');
+    this.translateService.addLangs(['es_MX', 'en_US']);
+    this.langs = this.translateService.getLangs();
+  }
+
+  public _toggleSidebar() {
     this._opened = !this._opened;
+  }
+
+  public changeLang(lang: string) {
+    this.translateService.use(lang);
+    localStorage.setItem('lang', lang);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './config/app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SidebarModule } from 'ng-sidebar';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent],
@@ -13,9 +20,23 @@ import { WelcomeComponent } from './pages/welcome/welcome.component';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
+    LoadingBarHttpClientModule,
+    LoadingBarRouterModule,
+    LoadingBarModule,
     SidebarModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/locales/', '.json');
+}
