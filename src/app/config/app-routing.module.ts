2d2deb24e94d31b310705e29104316f4a67879b1
Sawako <saturnoxspace@outlook.com>
2020-02-12 17:53:39 -0600

    [REFACTOR]: Remove unnecesary code.

--- src/app/config/app-routing.module.ts
+++ src/app/config/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     loadChildren: () =>
       import('../pages/dashboard/dashboard.module').then(
         m => m.DashboardModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,24 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require('@tailwindcss/custom-forms')],
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+    require('tailwindcss-grid')({
+      grids: [2, 3, 4, 5, 6, 8, 10, 12],
+      gaps: {
+        0: '0',
+        3: '0.5rem',
+        4: '1rem',
+        8: '2rem',
+        '4-x': '1rem',
+        '4-y': '1rem',
+      },
+      autoMinWidths: {
+        '16': '4rem',
+        '24': '6rem',
+        '300px': '300px',
+      },
+      variants: ['responsive'],
+    }),
+  ],
 };
